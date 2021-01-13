ca73e5e3db3f54d53af29b9c151a15bf94ba7913
AnthonyStembreit <stembreit@gmail.com>
2021-01-13 13:06:27 -0600

    changed title for tab, set mobile media screen styles

--- client/src/components/outerFooter/index.js
+++ client/src/components/outerFooter/index.js
@@ -7,13 +7,13 @@ export default function OuterFooter() {
     return (
         <Navbar sticky="bottom" id="outsideFooter">
             <Row id="tableFooter">
-                <Col lg={3}><img src="https://cerbanimo.quinixdesign.com/v2/wp-content/uploads/2020/11/Cerbanimo-Logo-V001-5.png" alt="Cerbanimo logo"></img> <br></br> <p>Copyright © 2020 Cerbanimo, LLC.</p></Col>
-                    <Col>
+                <Col lg={3} sm={12}><img id="footerLogo" src="https://cerbanimo.quinixdesign.com/v2/wp-content/uploads/2020/11/Cerbanimo-Logo-V001-5.png" alt="Cerbanimo logo"></img> <br></br> <p>Copyright © 2020 Cerbanimo, LLC.</p></Col>
+                    <Col >
                         <p className="footerTitle">Platform</p>
                         <p><Link to="/whatFor">What's It For?</Link></p>
                         <p><Link to="/whatDo">What it Does</Link></p>
                     </Col>
-                    <Col>
+                    <Col >
                         <p className="footerTitle">About</p>
                         <p><Link to="/vision">Our Vision</Link></p>
                         <p><Link to="/team">Our Team</Link></p>
